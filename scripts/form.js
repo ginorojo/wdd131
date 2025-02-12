6343c1ecb4c06e99b74df5489acfb0d13818b18d
ginorojo <ginorojoj@gmail.com>
2025-02-11 22:48:43 -0300

    form assigment

---
+++ scripts/form.js
@@ -0,0 +1,74 @@
+const currentYear = new Date().getFullYear();
+document.getElementById("year").innerHTML = currentYear;
+
+const lastModified = document.lastModified;
+document.getElementById("modification").innerHTML = lastModified;
+
+const mainNav = document.querySelector('.navigation');
+const navbutton = document.querySelector('#menu');
+
+navbutton.addEventListener('click', () => {
+  mainNav.classList.toggle('show');
+  navbutton.classList.toggle('show');
+});
+
+const products = [
+  {
+    id: "fc-1888",
+    name: "flux capacitor",
+    averagerating: 4.5
+  },
+  {
+    id: "fc-2050",
+    name: "power laces",
+    averagerating: 4.7
+  },
+  {
+    id: "fs-1987",
+    name: "time circuits",
+    averagerating: 3.5
+  },
+  {
+    id: "ac-2000",
+    name: "low voltage reactor",
+    averagerating: 3.9
+  },
+  {
+    id: "jj-1969",
+    name: "warp equalizer",
+    averagerating: 5.0
+  }
+];
+
+
+document.addEventListener("DOMContentLoaded", function () {
+  const productSelect = document.getElementById('product');
+
+  if (productSelect) {
+      products.forEach(product => {
+          const option = document.createElement('option');
+          option.value = product.name;
+          option.textContent = product.name;
+          productSelect.appendChild(option);
+      });
+  } 
+});
+
+function updateReviewCounter() {
+  let reviewCount = localStorage.getItem("reviewCounter") || 0;
+  reviewCount = parseInt(reviewCount); 
+  let counter = document.getElementById("counter");
+  counter.textContent = reviewCount; 
+}
+
+updateReviewCounter(); 
+
+function incrementReviewCounter() {
+  let reviewCount = localStorage.getItem("reviewCounter") || 0;
+  reviewCount = parseInt(reviewCount) + 1; 
+  localStorage.setItem("reviewCounter", reviewCount);
+  document.getElementById("counter").textContent = reviewCount;
+}
+
+
+
